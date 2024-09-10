4e532ad27111c7f72cbee12d90ff9f601bac3822
nadinnadiati <232410237@smkn1padaherang.sch.id>
2024-09-10 09:56:34 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// toggle & Responsive Navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navList = documenr.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navList.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
+
+
+//Clear form before unload
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("from")) {
+        form.request();
+    }
+};
